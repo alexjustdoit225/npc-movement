bdc6ebd214578c4b202de478e2fcc2e269e11004
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-02-21 10:41:29 -0600

    Update script.js
    -setup

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log('Hello World'); 
+const canvas = document.getElementById("canvas1"); 
+const c = canvas.getContext("2d");  
+const CANVAS_WIDTH = canvas.width = 600; 
+const CANVAS_HEIGHT = canvas.height = 600; 
