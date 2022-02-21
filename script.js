53cb198787d454f32c4ec05d5f3756c2ca4be9e3
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-02-21 12:30:43 -0600

    filnished main sprite movement animation

--- script.js
+++ script.js
@@ -1,4 +1,54 @@
+/** @type {HTMLCanvasElement} */
 const canvas = document.getElementById("canvas1"); 
 const c = canvas.getContext("2d");  
-const CANVAS_WIDTH = canvas.width = 600; 
-const CANVAS_HEIGHT = canvas.height = 600; 
+const CANVAS_WIDTH = canvas.width = 500; 
+const CANVAS_HEIGHT = canvas.height = 1000; 
+const numberOfEnemies = 100; 
+const enemiesArray = []
+
+let gameFrame = 0; 
+
+class Enemy {
+    constructor() {
+        this.image = new Image(); 
+        this.image.src = "/assets/enemy1.png";
+        // this.speed = Math.random() * 4 - 2; 
+        this.spriteWidth = 293; 
+        this.spriteHeight = 155; 
+        this.width = this.spriteWidth / 2.5;  
+        this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width); 
+        this.y = Math.random() * (canvas.height - this.height); 
+        this.frame = 0; 
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1); 
+    }
+    update() {
+    
+        this.x += Math.random() * 5 - 2.5; 
+        this.y += Math.random() * 3 - 2.5;
+        // animate sprites
+        //if:only run code every two loops ; this slows down the animation
+        if (gameFrame % this.flapSpeed === 0){
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+    draw() {
+        c.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, 
+        this.spriteHeight, this.x, this.y, this.width, this.height); 
+    }
+};
+// const enemy1 = new Enemy();
+for (let i = 0; i < numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy()); 
+};
+
+function animate() {
+    c.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); 
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw(); 
+    });
+    gameFrame++; 
+    requestAnimationFrame(animate)
+}
+animate(); 
